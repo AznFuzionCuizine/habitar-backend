dd23a5a43d3aa269f066d82951c440c886bab0aa
jeromerrr <jerome.shep@gmail.com>
2018-10-04 09:40:45 -0700

    created branhc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :habits
 devise_for :users, defaults: { format: :json }
+
+resources :users do
+  resources :habits
+end
+
+get "/users/:user_id/habits" => 'habits#index'
 end
